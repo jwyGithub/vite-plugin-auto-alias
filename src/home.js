ab75e2eb605281c7e4f2c0a191a380b7f4a7008c
江伟业 <jiangweiye@outlook.com>
2020-01-16 20:59:51 +0800

    测试sass

---
+++ src/home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './home.scss'
+export default class home extends Component {
+    render() {
+        return (
+            <div>
+                <h3>测试sass</h3>
+            </div>
+        )
+    }
+    
+}
