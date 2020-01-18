de318ab6be82bcd7396e3e210763d7aa785a2fe3
江伟业 <jiangweiye@outlook.com>
2020-01-18 12:07:39 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-// import App from './App';
-import Home from './home'
 
-ReactDOM.render(<Home />, document.getElementById('root'));
+//默认css样式
+import './index.css';
+import App from './views/App'
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
