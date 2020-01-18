cf4cde6b2f966af91a46fc8318435f6262ea6796
江伟业 <jiangweiye@outlook.com>
2020-01-18 12:07:58 +0800

    add

---
+++ src/views/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+    render() {
+        return (
+            <div className="app">
+                <h3>122</h3>
+            </div>
+        )
+    }
+}
---
+++ src/views/Chat/Chat.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import './Chat.scss'
+export default class Chat extends Component {
+    render() {
+        return (
+            <div className="chat">
+                
+            </div>
+        )
+    }
+}
